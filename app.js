72e0325ae9a53b4807c49cba9b2eacead8c59a28
Joe Linogao <linogaoj@tcd.ie>
2025-05-04 19:52:11 +0100

    setup layout and main public pages

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // Entry point for our MPA application
 // Import pacakges as needed
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
+// import our routes
 const indexRouter = require('./routes/index')
+const productsRouter = require('./routes/products')
+const aboutRouter = require('./routes/about')
 
 // setup express server with ejs templating
 const app = express()
@@ -12,8 +17,14 @@ app.use(express.json()) // let's us handle json data
 app.use(express.urlencoded({ extended: true })) // lets us handle url query strings
 app.use(express.static('public')) // lets us serve static files from public folder
 
+// setup main layout page so we can carry over head, navbar, and footer
+app.use(expressLayouts)
+app.set('layout', './layouts/baseLayout') // default layout to use
+
 // setup routing from our routes file
 app.use('/', indexRouter) // This allows us to handle GET, POST, etc... on specific routes
+app.use('/products', productsRouter)
+app.use('/about', aboutRouter)
 
 // start the server
 app.listen(port, () => {
---
+++ routes/products.js
@@ -0,0 +1,12 @@
+// Handle GET, POST, PUT, DELETE requests that are pointed towards index
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    // render our view
+    res.render('products', { title: 'HomeBrewedTCG - Products' })
+})
+
+// export our index.js route to app.js
+module.exports = router
