151361229f046b8b192281d951faac321341a2f5
Joe Linogao <linogaoj@tcd.ie>
2025-05-14 21:06:11 +0100

    b

--- routes/products.js
+++ routes/products.js
@@ -8,34 +8,8 @@ router.get('/', (req, res) => {
     res.render('products', { title: 'Products' })
 })
 
-router.get('/cards', (req, res) => {
-    // render our view
-    res.send([
-        {
-            name: 'Item 1',
-            description: 'This is item 1',
-            price: 10.0,
-        },
-        {
-            name: 'Item 2',
-            description: 'This is item 2',
-            price: 20.0,
-        },
-        {
-            name: 'Item 3',
-            description: 'This is item 3',
-            price: 30.0,
-        },
-    ])
-})
-
-router.get('/card', (req, res) => {
-    // render our view
-    res.send({
-        name: 'Item 1',
-        description: 'This is item 1',
-        price: 10.0,
-    })
+router.get('/pokemon/:type', (req, res) => {
+    res.send(req.params)
 })
 
 // export our index.js route to app.js
