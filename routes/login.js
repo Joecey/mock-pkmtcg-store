5fb4fd8b7955013c95ad5824cdf9a85515c098bc
Joe Linogao <linogaoj@tcd.ie>
2025-05-19 21:36:43 +0100

    added mock protected paths

--- routes/login.js
+++ routes/login.js
@@ -8,5 +8,18 @@ router.get('/', (req, res) => {
     res.render('login', { title: 'Login' })
 })
 
+router.get('/profile', (req, res) => {
+    // render our view
+    res.render('account', { title: 'My Account' })
+})
+
+router.post('/check-auth', (req, res) => {
+    console.log(req.body)
+    res.status(200).json({ success: true })
+
+    // If not authorized:
+    res.status(401).json({ error: 'Unauthorized' })
+})
+
 // export our index.js route to app.js
 module.exports = router
